18776954964e0e684960ad89dad5da231de146ed
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2020-09-28 19:43:30 +0300

    Change makefile to control the directories

--- workflow_template/src/constants.py
+++ workflow_template/src/constants.py
@@ -5,3 +5,8 @@ from os.path import dirname, join
 
 WORKFLOW_ROOT = dirname(dirname(__file__))
 LOGGING_PATH = join(WORKFLOW_ROOT, "logs")
+OUTPUT_PATH = join(WORKFLOW_ROOT, "results")
+DATA_PATH = join(WORKFLOW_ROOT, "data")
+TMP_PATH = join(WORKFLOW_ROOT, "tmp")
+
+DATABASE_FILE = join(DATA_PATH, "cpdp.db")
